a28b4b407d9b7421c19c2a939abe6da8b4f00239
onlydead796 <onlydead@protonmail.com>
2025-07-19 13:23:38 +0300

    main.py

--- main.py
+++ main.py
@@ -280,4 +280,4 @@ def check_license():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8080)), debug=True)
